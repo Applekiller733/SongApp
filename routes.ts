eb9590685dabfea4496b39a4f5a66d513c5ce603
936 - Popa-Zelu-Andrei <72962481+Applekiller733@users.noreply.github.com>
2025-08-13 13:46:54 +0300

    Delete routes.ts

--- routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, index, route, layout, prefix } from "@react-router/dev/routes";
-
-export default [
-    index("routes/home.tsx"),
-    route("/login", "routes/login/login.tsx")
-
-] satisfies RouteConfig;
